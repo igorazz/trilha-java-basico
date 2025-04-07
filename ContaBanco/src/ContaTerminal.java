9c006134b644490e5ecefca589f0875812fe379c
igorazzz <igor-azevedo12@hotmail.com>
2025-04-07 20:53:39 -0300

    segundo commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // Inferface do usuário
+    
+    System.out.println("Por favor, digite o número da Conta: ");
+    int numero = sc.nextInt();
+    sc.nextLine();
+
+    System.out.println("Por favor, digite o número da Agência: ");
+    String agencia = sc.nextLine();
+
+    System.out.println("Ditige o seu Nome: ");
+    String nomeCliente = sc.nextLine();
+
+
+    System.out.println("Por favor, digite o saldo inicial:");
+    double saldo = sc.nextDouble();
+
+    // Exibindo a mensagem final
+    System.out.println("\nOlá " + nomeCliente + 
+                       ", obrigado por criar uma conta em nosso banco. " +
+                       "Sua agência é " + agencia + 
+                       ", conta " + numero + 
+                       " e seu saldo " + saldo + " já está disponível para saque.");
+
+    // Fechando o Scanner
+    sc.close();
+
     }
 }
